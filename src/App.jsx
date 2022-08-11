bf9455b3923eaad84065449223639ffa9c26d58f
James <jamestariga08@live.com>
2022-08-10 17:43:03 -0700

    ✨ Initialized react-query

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Home from './pages/Home/'
 import Shows from './pages/Shows/'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  })
+
   return (
     <>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} exact></Route>
-          <Route path='/shows' element={<Shows />}></Route>
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} exact></Route>
+            <Route path='/shows' element={<Shows />}></Route>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </>
   )
 }
