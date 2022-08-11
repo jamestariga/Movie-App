97de37ede68ab8b67ed78b3c95d87927f99b93aa
James <jamestariga08@live.com>
2022-08-10 17:43:37 -0700

    ✨ Created a side navigation

---
+++ src/components/SideNavigation/SideNavigation.jsx
@@ -0,0 +1,11 @@
+import { BiHomeCircle } from 'react-icons/bi'
+import { AiOutlineCompass, AiOutlineCloud } from 'react-icons/ai'
+import { BsPeople } from 'react-icons/bs'
+import { RiSearchLine, RiLogoutBoxRLine } from 'react-icons/ri'
+import { BsGear } from 'react-icons/bs'
+
+const SideNavigation = () => {
+  return <div>SideNavigation</div>
+}
+
+export default SideNavigation
