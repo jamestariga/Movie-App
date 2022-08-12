26cd40a3a4c136e4288609e8d60a894a49018d75
James <jamestariga08@live.com>
2022-08-11 21:48:32 -0700

    🔥 Updated the position to fixed and added a scroll event listener

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,45 @@
+import { useEffect, useState } from 'react'
 import { FaChevronDown, FaChevronRight } from 'react-icons/fa'
 import { HiOutlineMenuAlt1 } from 'react-icons/hi'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth > 1020) {
+        // if user scrolls more than 40px
+        if (window.scrollY > 40) {
+          setIsScrolled(true)
+        } else {
+          setIsScrolled(false)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <>
-      <div className='navbar bg-base-100 py-10'>
+      <nav
+        className={`navbar py-6 fixed transition ${
+          isScrolled ? `bg-blue-600` : `bg-base-100`
+        }`}
+      >
         <div className='navbar-start'>
           <div className='dropdown'>
-            <label className='btn btn-ghost lg:hidden'>
+            <label tabIndex='0' className='btn btn-ghost lg:hidden'>
               <HiOutlineMenuAlt1 className='h-6 w-6 text-blue-500' />
             </label>
-            <ul className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'>
-              <li>
+            <ul
+              tabIndex='0'
+              className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+            >
+              <li tabIndex='0'>
                 <a>Home</a>
               </li>
               <li>
@@ -37,13 +64,13 @@ const NavBar = () => {
               </li>
             </ul>
           </div>
-          <a className='btn btn-ghost text-3xl ml-4 text-white xl:ml-20'>
+          <a className='btn btn-ghost text-3xl ml-4 text-white xl:ml-12'>
             Flixer
           </a>
         </div>
         <div className='navbar-center hidden lg:flex lg:w-5/6'>
           <ul className='menu menu-horizontal p-0 text-xl font-bold space-x-6'>
-            <li>
+            <li tabIndex='0'>
               <NavLink
                 to='/'
                 className={({ isActive }) =>
@@ -64,7 +91,11 @@ const NavBar = () => {
                 }
               >
                 TV Shows
-                <FaChevronDown className='h-4 w-4 text-blue-500' />
+                <FaChevronDown
+                  className={`h-4 w-4 ${
+                    isScrolled ? `text-blue-800` : `text-blue-500`
+                  }`}
+                />
               </NavLink>
               <ul className='p-2'>
                 <li>
@@ -77,7 +108,7 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-      </div>
+      </nav>
     </>
   )
 }
