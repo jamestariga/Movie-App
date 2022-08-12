12f9f46c048d846c1ff7acd691326a514bf45cf5
James <jamestariga08@live.com>
2022-08-12 12:49:03 -0700

    🔥 Created Cards component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const Card = () => {
-  return <div className='text-3xl'>Card</div>
+const Card = (props) => {
+  const { title, overview, backdrop_path, poster_path } = props
+
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        {backdrop_path ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+            alt={title}
+          />
+        ) : (
+          <img
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+            alt={title}
+          />
+        )}
+        <p>{overview}</p>
+      </div>
+    </>
+  )
 }
 
 export default Card
