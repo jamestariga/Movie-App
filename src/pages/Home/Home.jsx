a932e41a68f0d18ff71ed2226c37b554c642d026
James <jamestariga08@live.com>
2022-08-11 21:47:42 -0700

    🔥 Updated the layout of the Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
 import { useQuery } from '@tanstack/react-query'
 import { getTrendingMovies } from '../../api/movieApi'
+import SideNavigation from '../../components/SideNavigation/'
 
 const Home = () => {
   const { data, error, isLoading } = useQuery(['trending'], getTrendingMovies)
 
   return (
     <>
-      <div className='flex'>
-        <div className='flex grow justify-center w-1/6'>Menu</div>
-        <div className='flex grow flex-col justify-start w-4/6 px-10'>
+      <header className='flex'>
+        <div className='hidden lg:flex grow items-center flex-col w-auto pt-4'>
+          <div className='flex w-1/2'>
+            <h1 className='text-xl text-gray-400'>Menu</h1>
+          </div>
+          <div className='px-10'>
+            <SideNavigation />
+          </div>
+        </div>
+        <div className='flex grow flex-col justify-start w-3/6 px-10'>
           <h1 className='text-3xl text-white font-bold'>Trending Movie🔥</h1>
           <div className='flex flex-col justify-start w-full gap-4 pt-10'>
             {data?.map((movie) => (
@@ -19,7 +27,7 @@ const Home = () => {
           </div>
         </div>
         <div className='flex grow justify-center w-4/12'>Upcoming</div>
-      </div>
+      </header>
     </>
   )
 }
