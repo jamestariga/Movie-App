f764710f690cb79c3c3c31b9a338f85eb8d83429
James <jamestariga08@live.com>
2022-08-14 13:34:16 -0700

    🔥 Created a new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blurred: 'rgba(255, 255, 255, 0.25)',
+      },
+    },
   },
   daisyui: {
     themes: ['cupcake', 'dark'],
