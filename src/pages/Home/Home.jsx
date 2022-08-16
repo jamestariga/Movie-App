65f0570d1d890c2ee4a9032e008d9ab6031f0289
James <jamestariga08@live.com>
2022-08-15 20:47:09 -0700

    🔥 Carousel will only auto play when the window is visible

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,74 @@
+import { useState, useEffect, useRef } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getTrendingMovies } from '../../api/movieApi'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 import SideNavigation from '../../components/SideNavigation/'
-import Card from '../../components/Card/'
+import FeaturedCard from '../../components/FeaturedCard'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 import { Carousel } from 'react-responsive-carousel'
 
 const Home = () => {
   const { data, error, isLoading } = useQuery(['trending'], getTrendingMovies)
+  const [isVisible, setIsVisible] = useState(true)
+  const firstRenderRef = useRef(true)
+
+  useEffect(() => {
+    // Will only render once in development mode
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false
+      return
+    }
+
+    const listener = document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'visible') {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    })
+
+    return () => {
+      document.removeEventListener('visibilitychange', listener)
+    }
+  }, [])
+
+  const carouselOptions = () => {
+    return {
+      showThumbs: false,
+      showStatus: false,
+      showIndicators: false,
+      infiniteLoop: false,
+      centerMode: true,
+      emulateTouch: true,
+      autoPlay: isVisible ? true : false,
+      interval: 5000,
+      transitionTime: 1000,
+      width: '100%',
+      showArrows: true,
+      renderArrowPrev: (onClickHandler, hasPrev, label) =>
+        hasPrev && (
+          <button
+            type='button'
+            onClick={onClickHandler}
+            title={label}
+            className='bg-blurred absolute left-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
+          >
+            <FiChevronLeft className='h-6 w-6' />
+          </button>
+        ),
+      renderArrowNext: (onClickHandler, hasNext, label) =>
+        hasNext && (
+          <button
+            type='button'
+            onClick={onClickHandler}
+            title={label}
+            className='bg-blurred absolute right-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
+          >
+            <FiChevronRight className='h-6 w-6' />
+          </button>
+        ),
+    }
+  }
 
   return (
     <>
@@ -23,45 +84,10 @@ const Home = () => {
         <div className='flex grow flex-col lg:justify-start lg:items-start justify-center items-center w-full lg:w-3/6 px-6 lg:px-10'>
           <h1 className='text-3xl text-white font-bold'>Trending Movie🔥</h1>
           <div className='flex flex-col justify-start w-full gap-4 pt-10'>
-            <Carousel
-              autoPlay
-              centerMode
-              showArrows={true}
-              showIndicators={false}
-              showStatus={false}
-              showThumbs={false}
-              infiniteLoop={false}
-              transitionTime={1000}
-              interval={5000}
-              width={'100%'}
-              renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                hasPrev && (
-                  <button
-                    type='button'
-                    onClick={onClickHandler}
-                    title={label}
-                    className='bg-blurred absolute left-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
-                  >
-                    <FiChevronLeft className='h-6 w-6' />
-                  </button>
-                )
-              }
-              renderArrowNext={(onClickHandler, hasNext, label) =>
-                hasNext && (
-                  <button
-                    type='button'
-                    onClick={onClickHandler}
-                    title={label}
-                    className='bg-blurred absolute right-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
-                  >
-                    <FiChevronRight className='h-6 w-6' />
-                  </button>
-                )
-              }
-            >
+            <Carousel {...carouselOptions()}>
               {data?.map((movie) => (
                 <div key={movie.id} className='pr-8'>
-                  <Card {...movie} />
+                  <FeaturedCard {...movie} />
                 </div>
               ))}
             </Carousel>
