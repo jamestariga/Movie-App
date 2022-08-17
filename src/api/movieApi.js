1339afa55560b14b2228a0b9b54816546dc9f508
James <jamestariga08@live.com>
2022-08-16 18:56:11 -0700

    🔥 Created new API requests

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -4,14 +4,44 @@ const movieApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
 })
 
-export const getTrendingMovies = async () => {
-  const { VITE_APP_TMDB_KEY } = import.meta.env
+const { VITE_APP_TMDB_KEY } = import.meta.env
 
+export const getTrendingMovies = async () => {
   const response = await movieApi.get(
-    `/movie/upcoming?api_key=${VITE_APP_TMDB_KEY}&language=en-US`
+    `/movie/popular?api_key=${VITE_APP_TMDB_KEY}&language=en-US`
   )
 
   console.log(response.data.results)
 
   return response.data.results
+}
+
+export const getUpcomingMovies = async () => {
+  const response = await movieApi.get(
+    `/movie/upcoming?api_key=${VITE_APP_TMDB_KEY}&language=en-US`
+  )
+
+  const upcomingMovies = response.data.results.slice(
+    0,
+    response.data.results.length / 3
+  )
+
+  console.log(upcomingMovies)
+
+  return upcomingMovies
+}
+
+export const getTopRatedMovies = async () => {
+  const response = await movieApi.get(
+    `/movie/top_rated?api_key=${VITE_APP_TMDB_KEY}&language=en-US`
+  )
+
+  const topRatedMovies = response.data.results.slice(
+    0,
+    response.data.results.length / 4
+  )
+
+  console.log(topRatedMovies)
+
+  return topRatedMovies
 }
