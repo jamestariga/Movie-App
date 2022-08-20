97d09b4e5d09428cd3bcedd2ec743564c71e4bf0
James <jamestariga08@live.com>
2022-08-19 20:56:26 -0700

    🔥 Added the Side navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import SideNavigation from './components/SideNavigation'
 import Home from './pages/Home/'
 import Shows from './pages/Shows/'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -18,6 +19,7 @@ const App = () => {
       <QueryClientProvider client={client}>
         <Router>
           <NavBar />
+          <SideNavigation />
           <Routes>
             <Route path='/' element={<Home />} exact></Route>
             <Route path='/shows' element={<Shows />}></Route>
