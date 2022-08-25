607a248e51011522b710dec39b08d0ae6fe2da82
James <jamestariga08@live.com>
2022-08-24 18:11:08 -0700

    🔥 Featured Card now contains Genre list

--- src/components/FeaturedCard/FeaturedCard.jsx
+++ src/components/FeaturedCard/FeaturedCard.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
+import GenreID from './GenreID'
 
 const FeaturedCard = (props) => {
-  const { title, overview, backdrop_path, poster_path } = props
+  const { title, name, overview, backdrop_path, poster_path, genre_ids } = props
 
   const poster = `https://image.tmdb.org/t/p/original${poster_path}`
   const backdrop = `https://image.tmdb.org/t/p/original${backdrop_path}`
 
+  const displayGenre = () => {
+    const genre = genre_ids.map((genre) =>
+      GenreID.find((g) => g.value === genre)
+    )
+
+    return genre.map((g) => (
+      <div key={g.value}>
+        <div className='flex bg-blurred p-2'>
+          <h1 className='text-white text-sm'>{g.title}</h1>
+        </div>
+      </div>
+    ))
+  }
+
   return (
     <>
       <div
-        className='flex justify-start space-x-10 lg:w-auto w-full min-h-[5rem] h-[20rem] rounded-xl shadow-lg md:py-6 md:px-10 py-2 px-6 overflow-hidden'
+        className='flex justify-start space-x-10 lg:w-auto w-full min-h-[5rem] h-[20rem] rounded-xl shadow-lg md:py-6 md:px-10 py-2 px-6 overflow-hidden cursor-pointer'
         style={{
-          backgroundImage: `url(${backdrop})`,
+          backgroundImage: `url(${backdrop_path === null ? poster : backdrop})`,
           backgroundSize: 'cover',
         }}
       >
         <div className='hidden sm:flex max-w-xs w-2/5 2xl:2/5 4xl:w-3/12 shadow-xl rounded-3xl overflow-hidden'>
-          {backdrop_path ? (
-            <img src={poster} alt={title} className='w-full' />
+          {poster_path ? (
+            <img src={poster} alt={title || name} className='w-full' />
           ) : (
-            <img src={backdrop} alt={title} className='h-full' />
+            <img src={backdrop} alt={title || name} className='h-full' />
           )}
         </div>
         <div className='flex flex-col w-4/6 2xl:w-3/5 4xl:w-9/12 space-y-6'>
-          <h1 className='text-xl text-gray-100 font-extrabold'>{title}</h1>
+          <h1 className='text-xl text-gray-100 font-extrabold'>
+            {title || name}
+          </h1>
           <h1 className='text-sm text-gray-100 text-left'>{overview}</h1>
+          <div className='flex flex-wrap gap-4'>{displayGenre()}</div>
         </div>
       </div>
     </>
