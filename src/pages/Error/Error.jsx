b9879f6c6721572a7e440556f183e41ed79cab57
James <jamestariga08@live.com>
2022-08-24 19:10:43 -0700

    🔥 Created a temporary 404 page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center w-full h-screen'>
+        <h1 className='text-3xl text-white font-bold'>404</h1>
+        <h1 className='text-3xl text-white font-bold'>Page Not Found</h1>
+      </div>
+    </>
+  )
+}
+
+export default Error
