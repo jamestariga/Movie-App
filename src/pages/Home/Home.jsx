5323d9e2eaf26e3e25b21d6a00c27e0d23074133
James <jamestariga08@live.com>
2022-08-24 18:17:34 -0700

    🔥 Turned the Carousel to a component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,102 +1,108 @@
-import { useState, useEffect, useRef } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { getTrendingMovies } from '../../api/movieApi'
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
-import FeaturedCard from '../../components/FeaturedCard'
+import { useState } from 'react'
+import {
+  getTrendingMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+  getNowPlayingMovies,
+} from '../../api/movieApi'
+import {
+  getPopularShows,
+  getTopRatedShows,
+  getCurrentShows,
+  getAiringTodayShows,
+} from '../../api/showsApi'
+import useEventListener from '../../hooks/useEventListener'
+import Carousel from '../../components/Carousel'
 import Upcoming from '../../components/Upcoming'
 import TopRated from '../../components/TopRated'
-import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
-import { Carousel } from 'react-responsive-carousel'
 
 const Home = () => {
-  const { data, error, isLoading } = useQuery(['trending'], getTrendingMovies)
   const [isVisible, setIsVisible] = useState(true)
-  const firstRenderRef = useRef(true)
 
-  useEffect(() => {
-    // Will only render once in development mode
-    if (firstRenderRef.current) {
-      firstRenderRef.current = false
-      return
-    }
-
-    const listener = document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'visible') {
-        setIsVisible(true)
-      } else {
-        setIsVisible(false)
-      }
-    })
-
-    return () => {
-      document.removeEventListener('visibilitychange', listener)
-    }
-  }, [])
-
-  const carouselOptions = () => {
-    return {
-      showThumbs: false,
-      showStatus: false,
-      showIndicators: false,
-      infiniteLoop: false,
-      swipeable: true,
-      centerMode: true,
-      emulateTouch: true,
-      autoPlay: isVisible ? true : false,
-      interval: 5000,
-      transitionTime: 1000,
-      width: '100%',
-      showArrows: true,
-      renderArrowPrev: (onClickHandler, hasPrev, label) =>
-        hasPrev && (
-          <button
-            type='button'
-            onClick={onClickHandler}
-            title={label}
-            className='bg-blurred absolute left-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
-          >
-            <FiChevronLeft className='h-6 w-6' />
-          </button>
-        ),
-      renderArrowNext: (onClickHandler, hasNext, label) =>
-        hasNext && (
-          <button
-            type='button'
-            onClick={onClickHandler}
-            title={label}
-            className='bg-blurred absolute right-5 bottom-40 p-2 rounded-full text-white text-sm font-bold z-10'
-          >
-            <FiChevronRight className='h-6 w-6' />
-          </button>
-        ),
+  const handleVisibilityChange = () => {
+    if (document.visibilityState === 'visible') {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
     }
   }
+
+  useEventListener('visibilitychange', handleVisibilityChange)
 
   return (
     <>
       <header className='flex flex-wrap lg:space-x-10'>
         <div className='flex grow flex-col lg:justify-start lg:items-start justify-center items-center w-full xl:w-full 2xl:w-7/12 px-6 lg:px-10 pb-10'>
           <h1 className='text-3xl text-white font-bold'>Trending Movies🔥</h1>
-          <div className='flex flex-col justify-start w-full gap-4 pt-10'>
-            <Carousel {...carouselOptions()}>
-              {data?.map((movie) => (
-                <div key={movie.id} className='pr-8'>
-                  <FeaturedCard {...movie} />
-                </div>
-              ))}
-            </Carousel>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <Carousel
+              isVisible={isVisible}
+              isNeeded={true}
+              dataName='trendingMovies'
+              fetchData={getTrendingMovies}
+            />
           </div>
           <h1 className='text-3xl text-white font-bold pt-10'>
             Top Rated Movies ⭐
           </h1>
-          <div className='flex flex-col pt-4'>
-            <TopRated />
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <TopRated dataName='topRatedMovies' fetchData={getTopRatedMovies} />
+          </div>
+          <h1 className='text-3xl text-white font-bold pt-10'>
+            Now Playing 🎬
+          </h1>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <Carousel
+              isVisible={isVisible}
+              isNeeded={false}
+              dataName='nowPlayingMovies'
+              fetchData={getNowPlayingMovies}
+            />
+          </div>
+          <h1 className='text-3xl text-white font-bold pt-10'>
+            Popular Shows ⭐
+          </h1>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <Carousel
+              isVisible={isVisible}
+              isNeeded={false}
+              dataName='trendingShows'
+              fetchData={getPopularShows}
+            />
+          </div>
+          <h1 className='text-3xl text-white font-bold pt-10'>
+            Top Shows Today 🍿
+          </h1>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <TopRated dataName='topRatedShows' fetchData={getTopRatedShows} />
+          </div>
+          <h1 className='text-3xl text-white font-bold pt-10'>
+            Airing Shows Today 🍿
+          </h1>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <Carousel
+              isVisible={isVisible}
+              isNeeded={false}
+              dataName='airingTodayShows'
+              fetchData={getAiringTodayShows}
+            />
+          </div>
+          <h1 className='text-3xl text-white font-bold pt-10'>
+            Shows For You 📺
+          </h1>
+          <div className='flex flex-col justify-start w-full pt-10'>
+            <Carousel
+              isVisible={isVisible}
+              isNeeded={false}
+              dataName='currentShows'
+              fetchData={getCurrentShows}
+            />
           </div>
         </div>
         <div className='flex lg:justify-start lg:items-start justify-center items-center flex-col w-full 2xl:w-4/12 2xl:pr-10'>
           <h1 className='text-3xl text-white font-bold'>Upcoming Movies 🍿</h1>
           <div className='pt-10'>
-            <Upcoming />
+            <Upcoming fetchData={getUpcomingMovies} dataName='upcomingMovies' />
           </div>
         </div>
       </header>
