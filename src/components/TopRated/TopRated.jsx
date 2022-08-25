e9a6edf128829f61c42f3eeff0745fd3c5c3e464
James <jamestariga08@live.com>
2022-08-24 18:16:02 -0700

    🔥 Added a secondary props that takes in the query key

--- src/components/TopRated/TopRated.jsx
+++ src/components/TopRated/TopRated.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
-import { getTopRatedMovies } from '../../api/movieApi'
 
-const TopRated = () => {
-  const { data, error, isLoading } = useQuery(['topRated'], getTopRatedMovies)
+const TopRated = (props) => {
+  const { fetchData, dataName } = props
+  const { data, error, isLoading } = useQuery([dataName], fetchData)
 
   return (
     <>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:justify-center lg:items-center'>
         {data?.map((topRated) => (
           <div key={topRated.id} className='flex flex-col space-y-6 p-4'>
-            <div className='rounded-2xl overflow-hidden'>
+            <div className='rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
               {topRated.poster_path ? (
                 <img
                   src={`https://image.tmdb.org/t/p/original${topRated.poster_path}`}
