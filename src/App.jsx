27306e888fcee7d34f7320885e0b38906a4b0427
James <jamestariga08@live.com>
2022-08-27 00:33:27 -0700

    🔥 Added the ID component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout'
 import Home from './pages/Home/'
 import Shows from './pages/Shows/'
 import Error from './pages/Error/'
+import ID from './pages/ID/'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
             <Route path='/' element={<Layout />}>
               <Route path='/' element={<Home />} />
               <Route path='/shows' element={<Shows />} />
+              <Route path='/:id' element={<ID />} />
             </Route>
             <Route path='/*' element={<Error />} />
           </Routes>
