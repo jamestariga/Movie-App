1b8e6d9e93177486dba1bac2bde55bfabba51af8
James <jamestariga08@live.com>
2022-08-27 00:32:21 -0700

    🔥 Added a button that will redirect the user to the home page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
@@ -6,6 +6,11 @@ const Error = () => {
       <div className='flex flex-col justify-center items-center w-full h-screen'>
         <h1 className='text-3xl text-white font-bold'>404</h1>
         <h1 className='text-3xl text-white font-bold'>Page Not Found</h1>
+        <Link to='/'>
+          <button className='btn btn-ghost bg-blue-500 text-white font-bold py-2 px-4 rounded mt-10'>
+            Visit Our Home Page
+          </button>
+        </Link>
       </div>
     </>
   )
