5273ebe322298d6db435e559715d9ac06324af32
jamestariga <jamestariga08@live.com>
2022-08-31 08:09:41 -0700

    🔥 Added a Link that navigates to the ID page

--- src/components/TopRated/TopRated.jsx
+++ src/components/TopRated/TopRated.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 
 const TopRated = (props) => {
-  const { fetchData, dataName } = props
+  const { fetchData, dataName, media } = props
   const { data, error, isLoading } = useQuery([dataName], fetchData)
 
   return (
@@ -9,21 +10,23 @@ const TopRated = (props) => {
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:justify-center lg:items-center'>
         {data?.map((topRated) => (
           <div key={topRated.id} className='flex flex-col space-y-6 p-4'>
-            <div className='rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
-              {topRated.poster_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/original${topRated.poster_path}`}
-                  alt={topRated.title}
-                  className='w-[500px]'
-                />
-              ) : (
-                <img
-                  src={`https://image.tmdb.org/t/p/original${topRated.backdrop_path}`}
-                  alt={topRated.title}
-                  className='h-full'
-                />
-              )}
-            </div>
+            <Link to={`/${media}/${topRated.id}`}>
+              <div className='rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
+                {topRated.poster_path ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${topRated.poster_path}`}
+                    alt={topRated.title}
+                    className='w-[500px]'
+                  />
+                ) : (
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${topRated.backdrop_path}`}
+                    alt={topRated.title}
+                    className='h-full'
+                  />
+                )}
+              </div>
+            </Link>
           </div>
         ))}
       </div>
