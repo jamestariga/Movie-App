aaa9a5018a35eacf4a9b48ab5ded9128a590c30a
jamestariga <jamestariga08@live.com>
2022-09-09 13:17:01 -0700

    🔥 Updated the grid layout

--- src/components/Casts/Casts.jsx
+++ src/components/Casts/Casts.jsx
@@ -1,18 +1,20 @@
-import { useParams } from 'react-router-dom'
 import useQueryByID from '../../hooks/useQueryByID'
-import { getMovieByID, getCastByID } from '../../api/movieApi'
+import { getMovieCastByID } from '../../api/movieApi'
+import CastsCard from '../CastsCard'
 
 const Casts = ({ id }) => {
-  const {
-    data: cast,
-    isLoading: castLoading,
-    isError: castError,
-  } = useQueryByID('casts', id, getCastByID)
+  const { data, isLoading, isError } = useQueryByID(
+    'movieCasts',
+    id,
+    getMovieCastByID
+  )
 
   return (
-    <div className='flex flex-wrap '>
-      {cast?.map((cast) => (
-        <h3 key={cast.cast_id}>{cast.name}</h3>
+    <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 auto-rows-[1fr]'>
+      {data?.map((cast) => (
+        <div key={cast.cast_id}>
+          <CastsCard {...cast} />
+        </div>
       ))}
     </div>
   )
