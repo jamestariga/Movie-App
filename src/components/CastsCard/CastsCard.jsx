2d3a12b1b495d4577a791f4cb99cabbb45d30546
jamestariga <jamestariga08@live.com>
2022-09-09 21:35:31 -0700

    🔥 Minor update with the height and added a default for null profile_path

--- src/components/CastsCard/CastsCard.jsx
+++ src/components/CastsCard/CastsCard.jsx
@@ -1,26 +1,29 @@
 import Unknown from '../../../public/image/unknown.png'
+import { Link } from 'react-router-dom'
 
 const CastsCard = (props) => {
   const { name, character, profile_path } = props
 
   return (
     <>
-      <div className='flex flex-col space-y-6 p-4'>
-        <div className='w-1/2 rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
-          <img
-            src={
-              profile_path !== null
-                ? `https://image.tmdb.org/t/p/original${profile_path}`
-                : Unknown
-            }
-            alt={name}
-            className='min-h-[14rem] w-full'
-          />
+      <Link to={`/person/${props.id}`}>
+        <div className='flex flex-col items-center p-4'>
+          <div className='w-full lg:p-6 xl:p-8 rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
+            <img
+              src={
+                profile_path !== null
+                  ? `https://image.tmdb.org/t/p/original${profile_path}`
+                  : Unknown
+              }
+              alt={name}
+              className='max-h-[14rem] w-full rounded-2xl'
+            />
+          </div>
+          <h2>
+            <strong> {name} </strong> as {character}
+          </h2>
         </div>
-        <h2>
-          <strong> {name} </strong> as {character}
-        </h2>
-      </div>
+      </Link>
     </>
   )
 }
