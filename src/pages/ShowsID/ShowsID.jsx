3d4d974b8e69a15f8319b3db61169bb2d77e31e0
James <jamestariga08@live.com>
2022-09-24 16:24:01 -0700

    🔥 Fixed a minor bug

--- src/pages/ShowsID/ShowsID.jsx
+++ src/pages/ShowsID/ShowsID.jsx
@@ -9,43 +9,54 @@ const ShowsID = () => {
   const { data, isLoading, isError } = useQueryByID('showByID', id, getShowByID)
 
   const displayGenre = () => {
-    const genre = data?.genres.map((genre) => (
-      <div key={genre.id}>
-        <div className='flex bg-blurred p-2 hover:bg-blue-500 transition cursor-pointer'>
-          <h1 className='text-white text-sm'>{genre.name}</h1>
+    const genre = data?.genres
+      .filter(
+        (genre, index, self) =>
+          index === self.findIndex((t) => t.label === genre.label)
+      )
+      .map((genre) => (
+        <div key={genre.id}>
+          <div className='flex bg-blurred p-2 hover:bg-blue-500 transition cursor-pointer'>
+            <h1 className='text-white text-sm'>{genre.name}</h1>
+          </div>
         </div>
-      </div>
-    ))
+      ))
 
     return genre
   }
 
-  displayGenre()
+  const displayYear = () => {
+    const year = data?.first_air_date.split('-')[0]
+    return year
+  }
 
   return (
     <>
-      <header className='flex flex-col space-y-8'>
-        <div className='flex flex-col items-center justify-center w-full space-y-8'>
-          <div className='w-full sm:flex sm:w-1/2 shadow-xl sm:rounded-3xl overflow-hidden'>
-            <img
-              src={`https://image.tmdb.org/t/p/original${data?.backdrop_path}`}
-              alt={data?.name}
-              className='w-full'
-            />
-          </div>
-          <div className='flex flex-col px-4 sm:px-8 space-y-4'>
-            <h1 className='text-xl sm:text-center sm:text-2xl font-bold text-white'>
-              {data?.name}
-            </h1>
-            <div className='flex sm:justify-center flex-wrap gap-4'>
-              {displayGenre()}
+      <header className='flex flex-col'>
+        <div className='flex flex-col justify-center items-center px-8 space-y-8'>
+          <div className='flex flex-col xl:flex-row justify-center w-full 3xl:w-3/4 space-y-8 bg-blurred'>
+            <div className='w-full sm:flex xl:w-8/12 3xl:w-3/4 shadow-xl overflow-hidden'>
+              <img
+                src={`https://image.tmdb.org/t/p/original${data?.backdrop_path}`}
+                alt={data?.name}
+                className='w-full'
+              />
             </div>
-            <p className='text-sm sm:text-lg text-white'>{data?.overview}</p>
+            <div className='flex flex-col w-full xl:w-1/2 3xl:w-3/4 justify-start px-4 sm:p-8 space-y-4'>
+              <h1 className='text-xl sm:text-2xl font-bold text-white'>
+                {data?.name}
+              </h1>
+              <h1 className='text-xl font-bold text-white'>{displayYear()}</h1>
+              <div className='flex flex-wrap gap-4'>{displayGenre()}</div>
+              <p className='text-sm sm:text-base text-white'>
+                {data?.overview}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className='px-8'>
-          <h1 className='text-2xl font-bold text-white'>Casts:</h1>
-          <ShowCasts id={id} />
+          <div className='px-8'>
+            <h1 className='text-2xl font-bold text-white'>Casts:</h1>
+            <ShowCasts id={id} />
+          </div>
         </div>
       </header>
     </>
