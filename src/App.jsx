f49a4ea66da031bd9a70aa4bd534fac3bb0df2a3
James <jamestariga08@live.com>
2022-09-25 19:47:47 -0700

    🔥 Implemented the react loading skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import ID from './pages/ID/'
 import ShowsID from './pages/ShowsID'
 import Person from './pages/Person'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const App = () => {
   const client = new QueryClient({
@@ -20,18 +23,21 @@ const App = () => {
   return (
     <>
       <QueryClientProvider client={client}>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route path='/' element={<Home />} />
-              <Route path='/shows' element={<Shows />} />
-              <Route path='/Movie/:id' element={<ID />} />
-              <Route path='/Show/:id' element={<ShowsID />} />
-              <Route path='/person/:id' element={<Person />} />
-            </Route>
-            <Route path='/*' element={<Error />} />
-          </Routes>
-        </Router>
+        <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Layout />}>
+                <Route path='/' element={<Home />} />
+                <Route path='/shows' element={<Shows />} />
+                <Route path='/Movie/:id' element={<ID />} />
+                <Route path='/Show/:id' element={<ShowsID />} />
+                <Route path='/person/:id' element={<Person />} />
+              </Route>
+              <Route path='/*' element={<Error />} />
+            </Routes>
+          </Router>
+        </SkeletonTheme>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
   )
