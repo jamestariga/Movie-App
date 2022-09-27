2d1bf581b9bde109a2cb2200e44ba6fb2a59f4db
James <jamestariga08@live.com>
2022-09-26 17:51:35 -0700

    🔥 Added the Card Skeleton component

--- src/components/TopRated/TopRated.jsx
+++ src/components/TopRated/TopRated.jsx
@@ -1,17 +1,24 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useIsFetching } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
+import { SmallCardSkeleton } from '../CardSkeleton/CardSkeleton'
 
 const TopRated = (props) => {
   const { fetchData, dataName, media } = props
   const { data, error, isLoading } = useQuery([dataName], fetchData)
 
+  const isFetching = useIsFetching()
+
   return (
     <>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:justify-center lg:items-center'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:justify-center lg:items-start'>
+        {isLoading && isFetching && <SmallCardSkeleton cards={5} />}
         {data?.map((topRated) => (
-          <div key={topRated.id} className='flex flex-col space-y-6 p-4'>
+          <div
+            key={topRated.id}
+            className='flex flex-col space-y-6 p-4 h-full overflow-hidden w-full'
+          >
             <Link to={`/${media}/${topRated.id}`}>
-              <div className='rounded-2xl overflow-hidden hover:scale-[1.1] transition'>
+              <div className='rounded-2xl overflow-hidden hover:scale-[1.1] transition h-full'>
                 {topRated.poster_path ? (
                   <img
                     src={`https://image.tmdb.org/t/p/original${topRated.poster_path}`}
