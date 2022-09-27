03fa6dd88d4b71c85eda501f1ada0f2741378f7a
James <jamestariga08@live.com>
2022-09-26 17:51:52 -0700

    🔥 Added the Card Skeleton component

--- src/components/Upcoming/Upcoming.jsx
+++ src/components/Upcoming/Upcoming.jsx
@@ -1,15 +1,19 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useIsFetching } from '@tanstack/react-query'
 import UpcomingCard from '../../components/UpcomingCard'
+import { UpcomingCardSkeleton } from '../CardSkeleton/CardSkeleton'
 
 const Upcoming = (props) => {
-  const { fetchData, dataName } = props
+  const { fetchData, dataName, media } = props
   const { data, error, isLoading } = useQuery([dataName], fetchData)
+
+  const isFetching = useIsFetching()
 
   return (
     <>
+      {isLoading && isFetching && <UpcomingCardSkeleton cards={6} />}
       <div className='grid sm:grid-cols-2 2xl:flex 2xl:flex-col items-start justify-start'>
         {data?.map((upcoming) => (
-          <UpcomingCard key={upcoming.id} {...upcoming} />
+          <UpcomingCard key={upcoming.id} media={media} {...upcoming} />
         ))}
       </div>
     </>
